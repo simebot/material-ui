40c0f0f30e2cab90550fbb6c73cb03be18483e98
Simon Males <sime@sime.net.au>
2021-02-08 11:38:34 +0100

    Initial bojagi integration

---
+++ packages/material-ui/src/Button/Button.bojagi.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Button from './Button'; // your button implementation that you marked with @bojagi before
+
+export const primary = () => <Button variant="contained" color="primary">Primary</Button>;
+
+export const green = () => <Button variant="contained" color="secondary">Secondary</Button>;
